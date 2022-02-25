146467290b72c4aefda5cc29b628ac77d09f7520
tsonare <tsonare@bestpeers.com>
2022-02-25 14:29:21 +0530

    This is an new project of student study portal with updated models.

---
+++ dashboard/forms.py
@@ -0,0 +1,13 @@
+from  django import forms
+from .models import *
+
+
+class Notesform(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['title', 'description']
+
+
+
+    
+    
---
+++ dashboard/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from .models import Note
+from .forms import *
+from django.contrib import messages
+from django.views.generic import DetailView
+
+# Create your views here.
+
+
+def home_authenticated(request):
+    return render(request, 'dashboard/home_authenticated.html')
+
+
+def home_not_authenticated(request):
+    return render(request, 'dashboard/home_not_authenticated.html')
+
+
+def notes(request):
+    if request.method == "POST":
+        form = Notesform(request.POST)
+        if form.is_valid():
+            notes = Note(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+        messages.success(request, f"Note saved from {request.user.username} successfully.")
+        return redirect('notes')
+    else:
+        form = Notesform()
+    notes = Note.objects.filter(user=request.user)
+    context = {'notes': notes,'form': form}
+    return render(request, 'dashboard/notes.html', context)
+
+
+def delete_note(request,pk=None):
+    Note.objects.get(id=pk).delete()
+    messages.success(request, f"Note Deleted from {request.user.username} successfully.")
+    return redirect('notes')
+
+
+class NotesDetailView(DetailView):
+    model = Note
+    template_name = 'dashboard/notes_detail.html'
+
+
+def edit_note(request,pk):
+    note = Note.objects.get(id=pk)
+    form = Notesform(instance=note)
+    if request.method == "POST":
+        form = Notesform(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+        messages.success(request, f"Updated Note saved from {request.user.username} successfully.")
+        return redirect('notes')
+    context = {'form':form,}
+    return render(request, 'dashboard/edit_note.html', context)
+
+
